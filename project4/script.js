83be3c93c69b00741f9f4719b8c1dad1ec840422
Priscillaj65 <pj65@nmsu.edu>
2026-05-05 20:00:07 -0600

    Update script.js

--- project4/script.js
+++ project4/script.js
@@ -1,12 +1,8 @@
 document.getElementById("fanForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // stops page from refreshing
+    event.preventDefault();
 
-    // get user input
     let name = document.getElementById("name").value;
 
-    // create personalized message
-    let message = "Welcome to the BTS ARMY, " + name + "! 💜";
-
-    // display message on page
-    document.getElementById("message").textContent = message;
+    document.getElementById("message").textContent =
+        "Welcome to the BTS ARMY, " + name + "! 💜";
 });
